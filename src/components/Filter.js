c4da600e044602a33fa5664c93bd946357e5c92f
IronAdministrator <74884776+IronAdministrator@users.noreply.github.com>
2022-02-21 02:27:39 +0100

    add dropdown-filtering

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,6 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+import { useDetectOutsideClick } from "./useDetectOutsideClick";
 
 const Filter = ({ popular, setFiltered, activeGenre, setActiveGenre }) => {
+  const dropdownRef = useRef(null);
+  const [isActive, setIsActive] = useDetectOutsideClick(dropdownRef, false);
+  const onClick = () => setIsActive(!isActive);
+
   useEffect(() => {
     if (activeGenre === 0) {
       setFiltered(popular);
@@ -14,7 +19,50 @@ const Filter = ({ popular, setFiltered, activeGenre, setActiveGenre }) => {
 
   return (
     <div className="filter-container">
-      <button
+      <div className="menu-container">
+        <button onClick={onClick} className="menu-trigger">
+          <span>Genre: {activeGenre}</span>
+        </button>
+        <nav
+          ref={dropdownRef}
+          className={`menu ${isActive ? "active" : "inactive"}`}
+        >
+          <ul>
+            <li>
+              <button
+                className={activeGenre === 0 ? "active" : ""}
+                onClick={() => {
+                  setActiveGenre(0);
+                }}
+              >
+                All
+              </button>
+            </li>
+            <li>
+              <button
+                className={activeGenre === 35 ? "active" : ""}
+                onClick={() => {
+                  setActiveGenre(35);
+                }}
+              >
+                Comedy
+              </button>
+            </li>
+            <li>
+              <button
+                className={activeGenre === 28 ? "active" : ""}
+                onClick={() => {
+                  setActiveGenre(28);
+                }}
+              >
+                Action
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      {/* <button
         className={activeGenre === 0 ? "active" : ""}
         onClick={() => {
           setActiveGenre(0);
@@ -37,7 +85,7 @@ const Filter = ({ popular, setFiltered, activeGenre, setActiveGenre }) => {
         }}
       >
         Action
-      </button>
+      </button> */}
     </div>
   );
 };
